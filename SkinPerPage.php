2723b049b14369bae514fb50c88cc80b61da05d1
Kunal Mehta <legoktm@gmail.com>
2015-05-11 19:45:30 -0700

    Document functions and give them more descriptive names
    Change-Id: I3be0d6bea5d50ac5b9430294491c3f05a5af6811

--- SkinPerPage.php
+++ SkinPerPage.php
@@ -19,35 +19,54 @@ $wgMessagesDirs['SkinPerPage'] = __DIR__ . '/i18n';
 $wgExtensionMessagesFiles['SkinPerPage'] = dirname( __FILE__ ) . "/SkinPerPage.i18n.php";
 $wgExtensionMessagesFiles['SkinPerPageMagic'] = dirname( __FILE__ ) . '/SkinPerPage.i18n.magic.php';
 
-$wgHooks['ParserFirstCallInit'][] = 'SkinPerPage::setup';
-$wgHooks['OutputPageParserOutput'][] = 'SkinPerPage::outputHook';
+$wgHooks['ParserFirstCallInit'][] = 'SkinPerPage::onParserFirstCallInit';
+$wgHooks['OutputPageParserOutput'][] = 'SkinPerPage::onOutputPageParserOutput';
 
 class SkinPerPage {
-	static function setup( Parser $parser ) {
+	/**
+	 * Register our extension tag and parser function
+	 *
+	 * @param Parser $parser
+	 * @return bool
+	 */
+	public static function onParserFirstCallInit( Parser $parser ) {
 		# Old one for backwards compatibility
 		$parser->setHook( 'skin', array( __CLASS__, 'parserHook' ) );
 		# Function-style one that users should actually use
-		$parser->setFunctionHook( 'useskin', array( __CLASS__, 'outputHook2' ) );
+		$parser->setFunctionHook( 'useskin', array( __CLASS__, 'useskinHook' ) );
 		return true;
 	}
 
-	static function parserHook( $text, $attribs, $parser ) {
+	/**
+	 * Handler for <skin>foo</skin>
+	 */
+	public static function parserHook( $text, $attribs, Parser $parser ) {
 		$parser->mOutput->spp_skin = $text;
 		return '';
 	}
-	static function outputHook( $out, $parserOutput ) {
+
+	/**
+	 * Reads the skin key from ParserOutput and sets it as the skin to use for display
+	 *
+	 * @param OutputPage $out
+	 * @param ParserOutput $parserOutput
+	 * @return bool
+	 */
+	public static function onOutputPageParserOutput( OutputPage $out, ParserOutput $parserOutput ) {
 		if ( isset( $parserOutput->spp_skin ) ) {
-			SkinPerPage::renderHook( $parserOutput->spp_skin );
+			$skin = trim( strtolower( $parserOutput->spp_skin ) );
+			RequestContext::getMain()->setSkin( Skin::newFromKey( $skin ) );
 		}
 		return true;
 	}
 
-	static function outputHook2( $parser, $skin = '' ) {
+	/**
+	 * Handler for {{#useskin:foo}}
+	 *
+	 * @param Parser $parser
+	 * @param string $skin
+	 */
+	public static function useskinHook( Parser $parser, $skin = '' ) {
 		SkinPerPage::parserHook( $skin, array(), $parser );
-	}
-
-	static function renderHook( $skin ) {
-		$skin = trim( strtolower( $skin ) );
-		RequestContext::getMain()->setSkin( Skin::newFromKey( $skin ) );
 	}
 }
